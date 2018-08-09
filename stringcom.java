5d02da9965463ead0afbb18ec82f9ced70daf7f3
mannam-maneesha <41893993+mannam-maneesha@users.noreply.github.com>
2018-08-09 12:32:53 +0530

    Create stringcom.java

---
+++ stringcom.java
@@ -0,0 +1,18 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		String a,b;
+		Scanner sc=new Scanner(System.in);
+		a=sc.next();
+		b=sc.next();
+		System.out.println(a+b);// your code goes here
+	}
+}
